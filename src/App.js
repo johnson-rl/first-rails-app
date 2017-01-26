ab21fad700051eec9e0ca0a45a030fb3c67b17c8
johnson-rl <johnson.rl@gmail.com>
2017-01-25 23:33:33 -0800

    added blog post, seperated out comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 //
@@ -19,14 +19,37 @@ import React, { Component } from 'react';
 // }
 //
 // export default App;
-class Hello extends Component {
-    render(){
-      return (
-        <div>
-          <h1>Hello {this.props.name}</h1>
-          <p>You are {this.props.age} years old</p>
-        </div>
-      )
-    }
+// class Hello extends Component {
+//     render(){
+//       return (
+//         <div>
+//           <h1>Hello {this.props.name}</h1>
+//           <p>You are {this.props.age} years old</p>
+//         </div>
+//       )
+//     }
+// }
+// export default Hello
+
+import React, { Component } from 'react';
+import './App.css';
+import Comment from './Comment.js'
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.props.title}</h1>
+        <h3>written by {this.props.author}</h3>
+        <p>{this.props.body}</p>
+        <hr></hr>
+        <p>Comments:</p>
+        <ul>
+          <li><Comment body={this.props.comments[0]} /></li>
+        </ul>
+      </div>
+    )
+  }
 }
-export default Hello
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './App.js'
+import Post from './App.js'
 import './index.css';
 
+var post = {
+  title: "Why React is Soooooper Seeet",
+  author: "Ryan Johnson",
+  body: "React is basically the best because it allows you to make websites that React well to changing data and views",
+  comments: [
+    "Yup, this doood gets it",
+    "Sooooooopeer Seeeeeeeeet.",
+    "React is definitely not unseeet."
+  ]
+}
+
 ReactDOM.render(
-  <Hello name={"Nick"} age={24}/>,
+  <Post
+  title = {post.title}
+  author = {post.author}
+  body = {post.body}
+  comments = {post.comments}
+  />,
   document.getElementById('root')
 );
