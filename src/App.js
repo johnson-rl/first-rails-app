26307795da701454431ad93712816f69603e91e9
johnson-rl <johnson.rl@gmail.com>
2017-01-25 22:58:58 -0800

    first rails app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
+class Hello extends Component {
+    render(){
+      return (
+        <div>
+          <h1>Hello {this.props.name}</h1>
+          <p>You are {this.props.age} years old</p>
+        </div>
+      )
+    }
+}
+export default Hello
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './App.js'
+import './index.css';
+
+ReactDOM.render(
+  <Hello name={"Nick"} age={24}/>,
+  document.getElementById('root')
+);
